9d84e5cb57a6b08d3ea00bc46f78bc307f705cf7
Omar Nabi <onabi20@gmail.com>
2020-05-07 18:28:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,12 @@ require 'open-uri'
 
 html = open("https://www.nba.com/lakers/roster?ls=nav_team")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
-
-
-#courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
- 
-courses.each do |course|
-  puts course.text.strip
-end
+#doc = Nokogiri::HTML(open("https://flatironschool#.com/"))
+#doc.css(".headline-26OIBN").text
+#
+#
+##courses = doc.css("#2a778efd-1685-5ec6-9e5a#-0843d6a88b7b .inlineMobileLeft-2Yo002#.imageTextBlockGrid3-2XAK6G")
+# 
+#courses.each do |course|
+#  puts course.text.strip
+#end#
